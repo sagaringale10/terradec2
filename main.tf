b731806cfcdb9366478ff49e45f4da8f663ebd5a
sagaringale10 <sagaringale1@gmail.com>
2023-12-05 20:50:21 +0800

    ADDING TERRAFORM FILES

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-southeast-1"  # Set your desired AWS region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "sagar-terra-bucket"  # Set a unique name for your bucket
+
+  acl    = "private"  # Set the bucket access control, e.g., private, public-read, public-read-write
+
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Production"
+  }
+}
